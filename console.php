a30b2c6deaed14a09c04ea7874ee0c1126dc55e3
Jesus Urrutia <jesus.urrutia@gmail.com>
2014-09-23 10:19:08 -0300

    Added config file example and parse

--- console.php
+++ console.php
@@ -3,12 +3,14 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+use Symfony\Component\Yaml\Yaml;
 use JesusGoku\Application\TwitterAutomationApplication as Application;
 use JesusGoku\Command;
 
-$app = new Application('Twitter Automation', 'v0.1');
+$config = Yaml::parse(__DIR__ . '/config/config.yml');
+
+$app = new Application('Twitter Automation', 'v0.1', $config);
 
 $app->add(new Command\PostRandomTwitterCommand());
 
 $app->run();
-
